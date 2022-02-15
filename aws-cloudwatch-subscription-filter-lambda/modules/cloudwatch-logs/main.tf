6199dbb1c1de4ff1be1d47b54dd724d4f012910e
Duleendra Shashimal <duleendra@bambu.co>
2022-02-16 02:13:30 +0800

    Added cloudwatch module

---
+++ aws-cloudwatch-subscription-filter-lambda/modules/cloudwatch-logs/main.tf
@@ -0,0 +1,4 @@
+resource "aws_cloudwatch_log_group" "cloudwatch_filter_log_group" {
+  name = var.log_group_name
+  retention_in_days = var.log_retention_days
+}
---
+++ aws-cloudwatch-subscription-filter-lambda/modules/cloudwatch-logs/variables.tf
@@ -0,0 +1,8 @@
+variable "log_group_name" {
+  type = string
+}
+
+variable "log_retention_days" {
+  type = number
+  default = 30
+}
