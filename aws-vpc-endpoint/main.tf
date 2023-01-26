cd2cf91fd40e60fcf472fd624f9309db3b631d64
Duleendra Shashimal <duleendra@bambu.co>
2023-01-26 23:35:56 +0800

    Add vpc endpoint policy

--- aws-vpc-endpoint/main.tf
+++ aws-vpc-endpoint/main.tf
@@ -3,6 +3,8 @@ resource "aws_vpc_endpoint" "s3_endpoint" {
   vpc_id       = module.vpc.vpc_id
   vpc_endpoint_type = "Gateway"
   route_table_ids = module.vpc.private_route_table_ids
+  policy = data.aws_iam_policy_document.s3_gateway_endpoint_policy.json
+
 }
 
 resource "aws_vpc_endpoint" "sqs_endpoint" {
@@ -12,4 +14,23 @@ resource "aws_vpc_endpoint" "sqs_endpoint" {
   private_dns_enabled = true
   subnet_ids = module.vpc.private_subnets
   security_group_ids = [aws_security_group.interface_sg.id]
+}
+
+data "aws_iam_policy_document" "s3_gateway_endpoint_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:PutObject"
+    ]
+    resources = [
+      "arn:aws:s3:::du-emp-data",
+      "arn:aws:s3:::du-emp-data/*"
+    ]
+  }
 }
